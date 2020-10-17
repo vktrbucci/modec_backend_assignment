82a7db96ac74f018f83dd13341ea08459ba9ca2a
Victor Bucci <vr.bucci@gmail.com>
2020-10-16 22:54:20 -0300

    .gitignore, First view and app instalation.

--- modec_backend_assignment/urls.py
+++ modec_backend_assignment/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from equipment_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.index, name='index')
+    # path('active-equipment/', ),
 ]
